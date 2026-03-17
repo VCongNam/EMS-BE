e36204e06f4e21734814d087eac26265a85f6490
HNam2314 <152058268+HNam2314@users.noreply.github.com>
2026-03-17 19:29:28 +0700

    refactor: Update Assign TA to class
    - Refactor code of assign TA DTO

--- src/EMS.Application/Features/Classes/DTOs/AssignTADto.cs
+++ src/EMS.Application/Features/Classes/DTOs/AssignTADto.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace EMS.Application.Features.Classes.DTOs
 {
-    internal class AssignTADto
+    public class AssignTADto
     {
+        public Guid TAID { get; set; }
+        public string Permission { get; set; }
+        public decimal? SalaryPerSession { get; set; }
     }
 }
