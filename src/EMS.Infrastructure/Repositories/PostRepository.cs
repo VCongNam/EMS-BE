5ea1c9c6a0988d7621ba30b9c06213a0c1b5f469
KhueNTHE180887 <khuenthe180887@fpt.edu.vn>
2026-03-23 22:18:40 +0700

    update post

---
+++ src/EMS.Infrastructure/Repositories/PostRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EMS.Domain.Interfaces;
+
+namespace EMS.Infrastructure.Repositories
+{
+    public class PostRepository : IPostRepository
+    {
+    }
+}
