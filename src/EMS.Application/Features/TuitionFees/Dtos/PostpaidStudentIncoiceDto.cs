a6d4e8f039757a9035e64c9f3d55075a6904e04a
KhueNTHE180887 <khuenthe180887@fpt.edu.vn>
2026-04-15 02:09:24 +0700

    update

---
+++ src/EMS.Application/Features/TuitionFees/Dtos/PostpaidStudentIncoiceDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMS.Application.Features.TuitionFees.Dtos
+{
+    public class PostpaidStudentInvoiceDto
+    {
+        public Guid StudentId { get; set; }
+        public string StudentName { get; set; } = string.Empty;
+        public string? AvatarUrl { get; set; }
+        public Guid? InvoiceId { get; set; }
+        public int AttendedSessions { get; set; } // Số buổi đã hiện diện
+        public decimal TotalAmount { get; set; }
+        public decimal PaidAmount { get; set; }
+        public DateTime? DueDate { get; set; }
+        public string Status { get; set; } = string.Empty;
+    }
+}
---
+++ src/EMS.Application/Features/TuitionFees/Dtos/UpdateClassFeeConfigDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMS.Application.Features.TuitionFees.Dtos
+{
+    public class UpdateClassFeeConfigDto
+    {
+        public string BillingMethod { get; set; } = string.Empty;
+        public decimal TuitionFee { get; set; }
+        public int PaymentDeadlineDays { get; set; }
+    }
+}
